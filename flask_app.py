15be0c8126743e38c0f6503b6deb71efb230fe89
ngochuy9123 <100545140+ngochuy9123@users.noreply.github.com>
2023-09-11 11:57:59 +0700

    AI

--- flask_app.py
+++ flask_app.py
@@ -1,57 +1,105 @@
-from flask import Flask,render_template,Response,jsonify,request,session
-from ultralytics import YOLO
-import cv2
-import math
 
+# from flask import Flask, Response
+# from flask_socketio import SocketIO, emit, send
+# from ultralytics import YOLO
+# import json
+# import cv2
+
+# app = Flask(__name__)
+# socketio = SocketIO(app, cors_allowed_origins="*")
+
+# # @socketio.on('my event')
+# # def handle_connect(data):
+# #     print(data)
+
+# @socketio.on('test event')
+# def handle_testevent():
+#     emit('test event', "{data:" + "Hello" + "}")
+
+# def video_detection(path_x):
+#     with app.app_context():
+#         video_capture = path_x
+#         cap = cv2.VideoCapture(video_capture)
+#         model = YOLO("best.pt")
+#         classNames = ["go", "stop"]
+#         while True:
+#             success, img = cap.read()
+#             results = model(img, stream=True)
+#             for r in results:
+#                 boxes = r.boxes
+#                 for box in boxes:
+#                     class_name = classNames[int(box.cls[0])]
+#                     socketio.emit("test event", {'data': class_name})
+                    
+#             yield img
+
+# def generate_frames(path_x=''):
+#     yolo_output = video_detection(path_x)
+#     for detection in yolo_output:
+#         ref, buffer = cv2.imencode('.jpg', detection)
+#         frame = buffer.tobytes()
+#         yield (b'--frame\r\n'
+#                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# @app.route('/video')
+# def video():
+#     return Response(generate_frames(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# if __name__ == "__main__":
+#     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+
+
+
+
+from flask import Flask, Response
+from flask_socketio import SocketIO, emit, send
+from ultralytics import YOLO
+import json
+import cv2
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+# @socketio.on('my event')
+# def handle_connect(data):
+#     print(data)
+
+@socketio.on('test event')
+def handle_testevent():
+    emit('test event', "{data:" + "Hello" + "}")
 
 def video_detection(path_x):
-    video_capture = path_x
-    #Create a Webcam Object
-    cap=cv2.VideoCapture(video_capture)
-    frame_width=int(cap.get(3))
-    frame_height=int(cap.get(4))
-    #out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
-
-    model=YOLO("best.pt")
-    classNames = ["go", "stop"]
-    while True:
-        success, img = cap.read()
-        results=model(img,stream=True)
-        for r in results:
-            boxes=r.boxes
-            for box in boxes:
-                x1,y1,x2,y2=box.xyxy[0]
-                x1,y1,x2,y2=int(x1), int(y1), int(x2), int(y2)
-                print(x1,y1,x2,y2)
-                cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255),3)
-                conf=math.ceil((box.conf[0]*100))/100
-                cls=int(box.cls[0])
-                class_name=classNames[cls]
-                label=f'{class_name}{conf}'
-                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-                print(t_size)
-                c2 = x1 + t_size[0], y1 - t_size[1] - 3
-                cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
-                cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
-        yield img
-        #out.write(img)
-        #cv2.imshow("image", img)
-        #if cv2.waitKey(1) & 0xFF==ord('1'):
-            #break
-    #out.release()
+    with app.app_context():
+        video_capture = path_x
+        cap = cv2.VideoCapture(video_capture)
+        model = YOLO("best.pt")
+        classNames = ["go", "stop"]
+        while True:
+            _, img = cap.read()
+            results = model.predict(img, stream=True)
+            for r in results:
+                print(type(r))
+                for box in r.boxes:
+                    print(type(box))
+                    class_name = classNames[int(box.cls[0])]
+                    socketio.emit("test event", {'data': class_name})
+                    
+            yield img
 
 def generate_frames(path_x=''):
     yolo_output = video_detection(path_x)
     for detection in yolo_output:
-        ref,buffer = cv2.imencode('.jpg',detection)
-        frame= buffer.tobytes()
+        ref, buffer = cv2.imencode('.jpg', detection)
+        frame = buffer.tobytes()
         yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/video')
 def video():
-    #return Response(generate_frames(path_x='static/files/bikes.mp4'), mimetype='multipart/x-mixed-replace; boundary=frame')
-    return Response(generate_frames(path_x = 0),mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(generate_frames(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+
+
+
--- video.js
+++ video.js
@@ -1,53 +1,71 @@
-let videoStream; // Biến lưu trữ stream từ camera
-const videoClient = document.getElementById('videoClient');
+// let videoStream; // Biến lưu trữ stream từ camera
 const videoRobot = document.getElementById('videoRobot');
-const openButton = document.getElementById('openButton');
-const stopButton = document.getElementById('stopButton');
+
+var socket1 = io.connect('http://127.0.0.1:5000/');
+socket1.on('connect', function() {
+    socket1.emit('my event', {data: "Hello"})
+});
+
+socket1.on('test event', function(data) {
+    console.log(data['data']);
+    
+});
 
 
-function handleVideoStream(stream) {
-  // Gửi video từ client lên server
-  socket.emit('videoClient', stream);
-}
+// const socket1 = new WebSocket('ws://localhost:12345');  // Kết nối với máy chủ
 
-function openCamera() {
-  // Kiểm tra xem trình duyệt có hỗ trợ truy cập camera không
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Truy cập camera
-    navigator.mediaDevices.getUserMedia({ video: true })
-      .then(function(stream) {
-      // Gán stream từ camera vào thẻ video
-      videoClient.srcObject = stream;
-      videoStream = stream;
-      handleVideoStream(stream);   
-    })
-      .catch(function(error) {
-        console.error('Lỗi truy cập camera: ', error);
-      });
-  } else {
-    console.error('Trình duyệt không hỗ trợ truy cập camera.');
-  }
-  // Vô hiệu hóa nút "Open"
-  openButton.disabled = true;
-  // Kích hoạt nút "Close"
-  stopButton.disabled = false;
-}
+// // Khi nhận được dữ liệu từ máy chủ
+// socket1.addEventListener('message', function(event) {
+//     const class_name = event.data;
+//     document.getElementById('class_name').innerText = class_name;
+// });
 
-function stopCamera() {
-  if (videoStream) {
-    // Dừng stream từ camera
-    videoStream.getTracks().forEach(function(track) {
-      track.stop();
-    });
-    videoStream = null;
-    // Xóa stream từ thẻ video
-    videoClient.srcObject = null; 
-  }
-  // Vô hiệu hóa nút "Close"
-  stopButton.disabled = true;
-  // Kích hoạt nút "Open"
-  openButton.disabled = false;
-}
+
+// function handleVideoStream(stream) {
+//   // Gửi video từ client lên server
+//   socket.emit('videoClient', stream);
+// }
+
+// function openCamera() {
+//   // Kiểm tra xem trình duyệt có hỗ trợ truy cập camera không
+//   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//     // Truy cập camera
+//     navigator.mediaDevices.getUserMedia({ video: true })
+//       .then(function(stream) {
+//       // Gán stream từ camera vào thẻ video
+//       videoClient.srcObject = stream;
+//       videoStream = stream;
+//       handleVideoStream(stream);   
+//     })
+//       .catch(function(error) {
+//         console.error('Lỗi truy cập camera: ', error);
+//       });
+//   } else {
+//     console.error('Trình duyệt không hỗ trợ truy cập camera.');
+//   }
+//   // Vô hiệu hóa nút "Open"
+//   openButton.disabled = true;
+//   // Kích hoạt nút "Close"
+//   stopButton.disabled = false;
+// }
+
+
+
+// function stopCamera() {
+//   if (videoStream) {
+//     // Dừng stream từ camera
+//     videoStream.getTracks().forEach(function(track) {
+//       track.stop();
+//     });
+//     videoStream = null;
+//     // Xóa stream từ thẻ video
+//     videoClient.srcObject = null; 
+//   }
+//   // Vô hiệu hóa nút "Close"
+//   stopButton.disabled = true;
+//   // Kích hoạt nút "Open"
+//   openButton.disabled = false;
+// }
 
 // navigator.mediaDevices.getUserMedia({ video: true})
 //   .then(function(streamRobot) {
@@ -81,4 +99,3 @@ function stopCamera() {
 //   console.log('Đã đóng kết nối tới robot');
 // };
 
-            
