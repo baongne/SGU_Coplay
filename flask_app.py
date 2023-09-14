770a020b37be894a44c5dbc3ae608c7cf8ab56e5
Luisnguyen1 <103302594+Luisnguyen1@users.noreply.github.com>
2023-09-14 12:19:22 +0700

    update

--- flask_app.py
+++ flask_app.py
@@ -55,6 +55,7 @@ from flask_socketio import SocketIO, emit, send
 from ultralytics import YOLO
 import json
 import cv2
+import math
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -71,23 +72,29 @@ def video_detection(path_x):
     with app.app_context():
         video_capture = path_x
         cap = cv2.VideoCapture(video_capture)
-        model = YOLO("best.pt")
-        classNames = ["go", "stop"]
+        model=YOLO("modelAI7.pt")
+        classNames = ['bunch', 'c', 'hd1', 'hi', 'hura', 'jutsu', 'like', 'love']  #modelAI3
         # classNames = ['Hitsuji-Ram-', 'I-Boar-', 'Inu-Dog-', 'Mi-Snake-', 'Ne-Rat-', 'Saru-Monkey-', 'Tatsu-Dragon-', 'Tora-Tiger-', 'Tori-Bird-', 'U-Hare-', 'Uma-Horse-', 'Ushi-Ox-']
         while True:
             _, img = cap.read()
             results = model.predict(img, stream=True)
             for r in results:
-                print(type(r))
-                for box in r.boxes:
-                    print(type(box))
-                    class_name = classNames[int(box.cls[0])]                    
+                boxes = r.boxes
+                for box in boxes :
+                    x1,y1,x2,y2=box.xyxy[0]
+                    x1,y1,x2,y2=int(x1), int(y1), int(x2), int(y2)
+                    print(x1,y1,x2,y2)
+                    cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255),3)
+                    conf=math.ceil((box.conf[0]*100))/100
+                    cls=int(box.cls[0])
+                    class_name=classNames[cls]                   
                     socketio.emit("test event", {'data': class_name})
-                    # if(class_name == "go"):
-                    #     press('w')
-                    # if(class_name == "stop"):
-                    #     press('s')
-                    
+                    label=f'{class_name}{conf}'
+                    t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                    print(t_size)
+                    c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                    cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
+                    cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
                     
             yield img
 
